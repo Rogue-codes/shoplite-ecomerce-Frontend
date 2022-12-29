9e6d3096f50ab26b2188c056ab0dd9bf6e024625
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-12-29 09:33:05 +0100

    integrating production API

--- src/redux/ApiSlice.js
+++ src/redux/ApiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/shoplite",
+    baseUrl: "https://shoplite-pro.onrender.com/api/v1/shoplite",
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
